bbca1bd17c51d4713018a9b8a25718d26473c460
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-14 15:40:03 -0400

    Updating the function to create a new row of data, still working on it.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,9 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "674325522559",
     appId: "1:674325522559:web:c38d1dd8b3f5b620"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 let dataRef = firebase.database();
 
@@ -19,25 +19,25 @@ let trainFrequency = 3;
 let nextTrain = "11:00";
 let currentTime = moment();
 
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+console.log("CURRENT TIME: " + moment(curTime).format("hh:mm"));
 
 
-function generateTable() {
-
-
-
-};
-
-
-function calculateNext(first, freq) {
-    firstTrain = first;
-    freq = trainFrequency;
+// function generateTable() {
 
 
 
-};
+// };
 
-$("#add-user").on("click", function () {
+
+// function calculateNext(first, freq) {
+//     firstTrain = first;
+//     freq = trainFrequency;
+
+
+
+// };"button:#Get"
+
+$("button: #add-user").on("click", function () {
     event.preventDefault();
 
     let trainName = $("#name-input").val();
@@ -54,41 +54,46 @@ $("#add-user").on("click", function () {
         time: trainTime,
         frequency: tFrequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+    });
 
 
-      // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-    dataRef.ref().on("child_added", function(childSnapshot) {
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    dataRef.ref().on("child_added", function (childSnapshot) {
 
         // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().name);
+
         console.log(childSnapshot.val().name)
         console.log(childSnapshot.val().destination);
         console.log(childSnapshot.val().time);
         console.log(childSnapshot.val().frequency);
         console.log(childSnapshot.val().dateAdded);
-  
-        // full list of items to the well
-        $("#train-schedule").append("<tr>" +
-          "<td class='train-name>" + childSnapshot.val().name +
-          " </td><td class='train-destination'> " + childSnapshot.val().destination +
-          " </span><td class='train-time'> " + childSnapshot.val().time +
-          " </td><td class='train-frequency'> " + childSnapshot.val().frequency +
-          " </td></tr>");
-  
-        // Handle the errors
-      }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-      });
-  
-      dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        // Change the HTML to reflect
-        $("#name-display").text(snapshot.val().name);
-        $("#destination-display").text(snapshot.val().destination);
-        $("#time-display").text(snapshot.val().time);
-        $("#frequency-display").text(snapshot.val().frequency);
-      });
+
+        // Store everything into a variable.
+        var tName = childSnapshot.val().name;
+        var tDestination = childSnapshot.val().destination;
+        var tTime = childSnapshot.val().time;
+        var tNext = childSnapshot.val().frequency;
+
+
+        // Create the new row
+        let newRow = $("<tr>").append(
+
+            $("<td>").text(tName),
+            $("<td>").text(tDestination),
+            $("<td>").text(tTime),
+            $("<td>").text(tNext),
+
+        );
+
+        // Append the new row to the table
+        $("#train-schedule > tbody").append(newRow);
+    });
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+
   
 
-    
-});

94bcc751a96fd19fdbd2258264256cf1363c0e28
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-13 21:56:35 -0400

    Basically finished all the layout code, going to start on some javascript functions now.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+console.log("Working...");
+
+let firstTrain = "03:30";
+
+let trainFrequency = 3;
+
+var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    

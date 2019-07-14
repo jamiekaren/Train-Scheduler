0959aabc5156df668beb32585e4cd96f6efa590e
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-14 16:41:51 -0400

    currently able to grab values, console log them, and clear input areas.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,35 +9,13 @@ var firebaseConfig = {
     messagingSenderId: "674325522559",
     appId: "1:674325522559:web:c38d1dd8b3f5b620"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-let dataRef = firebase.database();
-
-let firstTrain = "03:30";
-let trainFrequency = 3;
-let nextTrain = "11:00";
-let currentTime = moment();
-
-console.log("CURRENT TIME: " + moment(curTime).format("hh:mm"));
+// let dataRef = firebase.database();
 
 
-// function generateTable() {
-
-
-
-// };
-
-
-// function calculateNext(first, freq) {
-//     firstTrain = first;
-//     freq = trainFrequency;
-
-
-
-// };"button:#Get"
-
-$("button: #add-user").on("click", function () {
+$("#add-user").click(function (event) {
     event.preventDefault();
 
     let trainName = $("#name-input").val();
@@ -47,53 +25,10 @@ $("button: #add-user").on("click", function () {
 
     console.log(trainName, currentDestination, trainTime, tFrequency);
 
-    dataRef.ref().push({
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
 
-        name: trainName,
-        destination: currentDestination,
-        time: trainTime,
-        frequency: tFrequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
-
-
-    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-    dataRef.ref().on("child_added", function (childSnapshot) {
-
-        // Log everything that's coming out of snapshot
-
-        console.log(childSnapshot.val().name)
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().time);
-        console.log(childSnapshot.val().frequency);
-        console.log(childSnapshot.val().dateAdded);
-
-        // Store everything into a variable.
-        var tName = childSnapshot.val().name;
-        var tDestination = childSnapshot.val().destination;
-        var tTime = childSnapshot.val().time;
-        var tNext = childSnapshot.val().frequency;
-
-
-        // Create the new row
-        let newRow = $("<tr>").append(
-
-            $("<td>").text(tName),
-            $("<td>").text(tDestination),
-            $("<td>").text(tTime),
-            $("<td>").text(tNext),
-
-        );
-
-        // Append the new row to the table
-        $("#train-schedule > tbody").append(newRow);
-    });
-
-    // Handle the errors
-}, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
 });
-
-
-  
 

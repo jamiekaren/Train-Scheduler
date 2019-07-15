a96055d5878779394734c55a87c4751e720554d0
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-07-15 18:00:02 -0400

    Finally got Firebase to work correctly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,57 @@
 console.log("Working...");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBuEWtCqtrfRqvthteUbGvYe39iyUgxjdI",
-    authDomain: "project1-fdec4.firebaseapp.com",
-    databaseURL: "https://project1-fdec4.firebaseio.com",
-    projectId: "project1-fdec4",
-    storageBucket: "",
-    messagingSenderId: "674325522559",
-    appId: "1:674325522559:web:c38d1dd8b3f5b620"
+let firebaseConfig = {
+  apiKey: "AIzaSyBuEWtCqtrfRqvthteUbGvYe39iyUgxjdI",
+  authDomain: "project1-fdec4.firebaseapp.com",
+  databaseURL: "https://project1-fdec4.firebaseio.com",
+  projectId: "project1-fdec4",
+  storageBucket: "",
+  messagingSenderId: "674325522559",
+  appId: "1:674325522559:web:c38d1dd8b3f5b620"
 };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Comment this for now please
 // let dataRef = firebase.database();
 
 
-$("#add-user").click(function (event) {
-    event.preventDefault();
+$("form").on("click", "button", function (event) {
+  event.preventDefault();
 
-    let trainName = $("#name-input").val();
-    let currentDestination = $("destination-input").val();
-    let trainTime = $("#time-input").val();
-    let tFrequency = $("#frequency-input").val();
+  //check on-click event is working
+  console.log("Form on click working!");
 
-    console.log(trainName, currentDestination, trainTime, tFrequency);
+  let trainName = $("#name-input").val();
+  let currentDestination = $("destination-input").val();
+  let trainTime = $("#time-input").val();
+  let tFrequency = $("#frequency-input").val();
 
-    $("#name-input").val("");
-    $("#destination-input").val("");
-    $("#time-input").val("");
-    $("#frequency-input").val("");
+  console.log(trainName, currentDestination, trainTime, tFrequency);
+
+  // Let's try this: 
+  firebase.database().ref('/').set({
+    name: trainName,
+    destination: currentDestination,
+    first: trainTime,
+    frequency: tFrequency
+  });
+
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#time-input").val("");
+  $("#frequency-input").val("");
+
+  // let newTrain = {
+  //     name: trainName,
+  //     destination: currentDestination,
+  //     first: trainTime,
+  //     frequency: tFrequency
+  //   };
+
+  // Uploads employee data to the database
+  //dataRef.ref().push(newTrain);
 
 });
 
